66d123541bdbe35658ec73447de7074c114001fa
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2022-08-02 19:35:57 +0400

    Changes

---
+++ Haier.cpp
@@ -0,0 +1,51 @@
+#include "Haier.h"
+
+uint8_t Frame::calc_crc8_() const {
+  uint8_t crc = 0;
+  if (this->buf_.size() > OFFSET_LENGTH) {
+    auto it = this->buf_.begin() + OFFSET_LENGTH;
+    const auto end = it + this->get_length_();
+    while (it != end)
+      crc += *it++;
+  }
+  return crc;
+}
+
+uint16_t Frame::calc_crc16_() const {
+  uint16_t crc = 0;
+  if (this->buf_.size() > OFFSET_LENGTH) {
+    auto it = this->buf_.begin() + OFFSET_LENGTH;
+    const auto end = it + this->get_length_();
+    while (it != end) {
+      crc ^= static_cast<uint16_t>(*it++);
+      for (size_t n = 0; n < 8; n++)
+        crc = (crc >> 1) ^ ((crc & 1) ? 0xA001 : 0x0000);
+    }
+  }
+  return crc;
+}
+
+void FrameReader::read() {
+  while (this->uart_->available() > 0) {
+    uint8_t data;
+    const auto idx = this->buf_.size();
+    this->uart_->read_byte(&data);
+    if (idx < OFFSET_LENGTH && data != 255) {
+      this->buf_.clear();
+      continue;
+    }
+    this->buf_.push_back(data);
+    if (idx >= 5 && (idx - 5) == this->get_length_()) {
+      if (this->is_valid_())
+        this->cb_(*this);
+      this->buf_.clear();
+    }
+  }
+}
+
+bool FrameReader::is_valid_() const {
+  const auto p = this->buf_.end();
+  const uint16_t crc16 = 256 * p[-2] + p[-1];
+  const uint8_t crc8 = p[-3];
+  return this->calc_crc8_() == crc8 && this->calc_crc16_() == crc16;
+}
--- Haier.h
+++ Haier.h
@@ -125,32 +125,9 @@ class Frame {
  protected:
   std::vector<uint8_t> buf_;
   static const size_t OFFSET_LENGTH = 2;
-
   size_t get_length_() const { return this->buf_[OFFSET_LENGTH]; }
-  uint8_t calc_crc8_() const {
-    uint8_t crc = 0;
-    if (this->buf_.size() > OFFSET_LENGTH) {
-      auto it = this->buf_.begin() + OFFSET_LENGTH;
-      const auto end = it + this->get_length_();
-      while (it != end)
-        crc += *it++;
-    }
-    return crc;
-  }
-
-  uint16_t calc_crc16_() const {
-    uint16_t crc = 0;
-    if (this->buf_.size() > OFFSET_LENGTH) {
-      auto it = this->buf_.begin() + OFFSET_LENGTH;
-      const auto end = it + this->get_length_();
-      while (it != end) {
-        crc ^= static_cast<uint16_t>(*it++);
-        for (size_t n = 0; n < 8; n++)
-          crc = (crc >> 1) ^ ((crc & 1) ? 0xA001 : 0x0000);
-      }
-    }
-    return crc;
-  }
+  uint8_t calc_crc8_() const;
+  uint16_t calc_crc16_() const;
 };
 
 class FrameReader : public Frame {
@@ -165,33 +142,12 @@ class FrameReader : public Frame {
   // Write other frame to UART
   void write(const Frame &frame) { this->uart_->write_array(frame.buf_); }
   // Read frame. Call callback function if valid frame was received.
-  void read() {
-    while (this->uart_->available() > 0) {
-      uint8_t x;
-      const size_t idx = this->buf_.size();
-      this->uart_->read_byte(&x);
-      if (idx < OFFSET_LENGTH && x != 255) {
-        this->buf_.clear();
-        continue;
-      }
-      this->buf_.push_back(x);
-      if (idx >= 5 && (idx - 5) == this->get_length_()) {
-        if (this->is_valid_())
-          this->cb_(*this);
-        this->buf_.clear();
-      }
-    }
-  }
+  void read();
 
  protected:
   UARTComponent *uart_{};
   callback cb_{};
-  bool is_valid_() const {
-    const auto p = this->buf_.end();
-    const uint16_t crc16 = 256 * p[-2] + p[-1];
-    const uint8_t crc8 = p[-3];
-    return this->calc_crc8_() == crc8 && this->calc_crc16_() == crc16;
-  }
+  bool is_valid_() const;
 };
 
 String getHex(uint8_t *message, uint8_t size) {
