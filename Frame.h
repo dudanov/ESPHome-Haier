ae069094e186cdece53a68eff070b3745dac618d
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2022-08-07 10:14:06 +0400

    Changes

--- Frame.h
+++ Frame.h
@@ -1,19 +1,52 @@
 #pragma once
 
 #include <deque>
+#include "esphome/core/helpers.h"
 #include "esphome/components/uart/uart.h"
 
 namespace esphome {
 namespace uart {
 
+template<typename T> T get_buffer_data_le(const uint8_t *buffer) {
+  T data = 0;
+  for (size_t n = 0; n < sizeof(T); n++)
+    data |= static_cast<T>(*buffer++) << (n * 8);
+  return data;
+}
+
+template<typename T> T get_buffer_data_be(const uint8_t *buffer) {
+  T data = 0;
+  for (size_t n = sizeof(T); n;)
+    data |= static_cast<T>(*buffer++) << (--n * 8);
+  return data;
+}
+
+template<typename T> void set_buffer_data_le(uint8_t *buffer, const T &data) {
+  for (size_t n = 0; n < sizeof(T); n++)
+    *buffer++ = static_cast<uint8_t>(data >> (n * 8));
+}
+
+template<typename T> void set_buffer_data_be(uint8_t *buffer, const T &data) {
+  for (size_t n = sizeof(T); n;)
+    *buffer++ = static_cast<uint8_t>(data >> (--n * 8));
+}
+
+template<typename T> void append_buffer_data_le(std::vector<uint8_t> &buffer, const T &data) {
+  for (size_t n = 0; n < sizeof(T); n++)
+    buffer.push_back(static_cast<uint8_t>(data >> (n * 8)));
+}
+
+template<typename T> void append_buffer_data_be(std::vector<uint8_t> &buffer, const T &data) {
+  for (size_t n = sizeof(T); n;)
+    buffer.push_back(static_cast<uint8_t>(data >> (--n * 8)));
+}
+
 class Frame {
   friend class FrameReader;
 
  public:
-  typedef std::vector<uint8_t> buffer_t;
-
  protected:
-  buffer_t data_;
+  std::vector<uint8_t> data_;
   bool get_bit(size_t idx, size_t nbit) const { return this->data_[idx] & (1 << nbit); }
   void set_bit(bool value, size_t idx, size_t nbit) {
     if (value)
@@ -28,11 +61,17 @@ class Frame {
     this->data_[idx] &= ~bitmask;
     this->data_[idx] |= (value << shift) & bitmask;
   }
+  template<typename T> T get_le(size_t idx) const { return get_buffer_data_le(&this->data_[idx]); }
+  template<typename T> T get_be(size_t idx) const { return get_buffer_data_be(&this->data_[idx]); }
+  template<typename T> void set_le(const T &data, size_t idx) { set_buffer_data_le(&this->data_[idx], data); }
+  template<typename T> void set_be(const T &data, size_t idx) { set_buffer_data_be(&this->data_[idx], data); }
+  template<typename T> void append_le(const T &data) { append_buffer_data_le(this->data_, data); }
+  template<typename T> void append_be(const T &data) { append_buffer_data_be(this->data_, data); }
 };
 
 class FrameReader : public Frame, public Component {
  public:
-  using on_receive_cb = std::function<bool(uint8_t, buffer_t &)>;
+  using on_receive_cb = std::function<bool(uint8_t, std::vector<uint8_t> &)>;
   using on_frame_cb = std::function<void(FrameReader &)>;
   // Set UART component
   void set_uart_parent(UARTComponent *parent) { this->uart_ = parent; }
@@ -48,10 +87,15 @@ class FrameReader : public Frame, public Component {
   void loop() override;
 
  protected:
+  std::deque<Frame *> write_queue_;
   UARTComponent *uart_{};
-  
   on_receive_cb on_receive_cb_;
   on_frame_cb on_frame_cb_;
+};
+
+class Protocol {
+  virtual bool on_data(uint8_t data, std::vector<uint8_t> &buffer) = 0;
+  virtual void on_frame(FrameReader &reader) = 0;
 };
 
 }  // namespace uart
